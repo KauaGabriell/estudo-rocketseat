d2c047f000706b1d0eca641522574456f9e32901
Kauã Gabriell <kauagabriell64@gmail.com>
2026-01-23 16:32:38 -0300

    Verificação de tipo estático.

---
+++ aula02.ts
@@ -0,0 +1,21 @@
+// const message = 'Hello Typescript';
+let message = 'Hello Type';
+
+//Consigo mudar?
+message = 'Typescritp';
+
+//Consigo executar?
+//message()
+function showMessage() {
+  console.log(message);
+}
+showMessage();
+
+const user = {
+  name: 'Peixe',
+  email: 'peixe@gmail.com',
+};
+
+// Propriedade existe?
+// console.log(user.avatar);
+console.log(user.email);

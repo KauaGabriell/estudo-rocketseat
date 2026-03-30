f1c5db6971f342f4e471fbd47ea68210f8c3a8f3
Kauã Gabriell <kauagabriell64@gmail.com>
2026-03-30 09:08:50 -0300

    Update

--- queryBuilder/src/server.ts
+++ queryBuilder/src/server.ts
@@ -4,18 +4,31 @@ const app = express();
 app.use(express.json());
 
 app.post('/courses', async (request: Request, response: Response) => {
-  const { name, description } = request.body;
+  const { name, description, duration_in_hours, certificate } = request.body;
 
-  // await knex('courses').insert({ name, description });
-  await knex.raw('INSERT INTO courses (name) VALUES (?)', [name]);
+  const course = await knex('courses')
+    .insert({ name, description, duration_in_hours, certificate })
+    .returning('*');
 
-  response.status(201).json();
+  response.status(201).json(course);
 });
 
 app.get('/courses', async (request: Request, response: Response) => {
   const courses = await knex('courses').select().orderBy('id', 'desc');
 
   response.json(courses);
+});
+
+app.put('/courses/:id', async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const { name } = request.body;
+
+  const course = await knex('courses')
+    .where({ id })
+    .update({ name })
+    .returning('*');
+
+  return response.json(course);
 });
 
 app.listen(3333, () => console.log(`Server is running on port 3333`));

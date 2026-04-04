b8ea36232ee0e8d8e17ace32552d29acc7d190d7
Kauã Gabriell <kauagabriell64@gmail.com>
2026-04-04 15:55:24 -0300

    Update com Prisma

--- orm/fullstack-orm-template-main/src/controllers/questions-controller.ts
+++ orm/fullstack-orm-template-main/src/controllers/questions-controller.ts
@@ -24,6 +24,16 @@ class QuestionsController {
   }
 
   async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { title, content } = request.body;
+    await prisma.question.update({
+      data: {
+        title,
+        content,
+      },
+      where: { id },
+    });
+
     return response.json();
   }
 
